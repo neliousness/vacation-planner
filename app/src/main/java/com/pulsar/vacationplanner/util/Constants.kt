36a98b2c52c768c603e448f56e7584499b6534ef
Nelio Lucas <nelio.lucas@icube-mz.com>
2024-10-11 22:38:39 +0200

    add ReadME

--- app/src/main/java/com/pulsar/vacationplanner/util/Constants.kt
+++ app/src/main/java/com/pulsar/vacationplanner/util/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val ONBOARDED = "onboarded"
     const val ITINERARY_RETRY_ATTEMPTS = 3
     const val DESTINATION_RETRY_ATTEMPTS = 2
-    const val BASE_URL = "http://10.0.2.2:8080/"
+    const val BASE_URL = "https://lynq-vacation-planner.onrender.com"
     const val EMPTY = ""
     const val DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
 }
